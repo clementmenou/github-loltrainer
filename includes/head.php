3bf3666cdf511f4e9817b7d46dfd78ba195db50b
Clément Menou <clementmenou0@gmail.com>
2023-10-03 19:06:47 +0200

    loltrainer 03/10/23 v5 index.php le retour

---
+++ includes/head.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title><?= $title ?></title>
+
+    <?php foreach($styles as $style): ?>
+        <link rel="stylesheet" href="styles/<?= $style ?>.css">
+    <?php endforeach;?>
+</head>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+    $title = "Lol trainer";
+    $styles = ['setup.css', 'menu.css', 'settings.css', 'credits.css',  'index.css'];
+
+    include 'includes/head.php';
+?>
+<body>
+    <?php include 'includes/menu.html';?>
+    <?php include 'includes/settings.html';?>
+    <?php include 'includes/credits.html';?>
+
+    <div class="board"></div>
+
+    <script src="scripts/index.js"></script>
+</body>
+</html>
